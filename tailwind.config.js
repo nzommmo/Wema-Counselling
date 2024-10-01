fe9129b2185f1de21cb802e1f1ff6a93a6b57e18
Maureen Njihia <maureennjhia468@gmail.com>
2024-10-01 11:41:25 +0300

    Setup project structure and install necessary dependecies

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+import flowbite from "flowbite-react/tailwind";
+
+import tailgrids from "tailgrids/plugin";
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'baloo': ['"Baloo Da 2"', 'sans-serif'],
+        'bruno': ['"Bruno Ace"', 'sans-serif']
+        // Add other font families as needed
+      },
+      backgroundImage: {}
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require("flowbite/plugin"),
+    [tailgrids],
+  ],
+});
